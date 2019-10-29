aae67877e429decc21b08eb3ae18179944374fd1
mediarcj <mediarcj@gmail.com>
2019-10-28 23:28:48 -1000

    initial commit

---
+++ content.js
@@ -0,0 +1,12 @@
+// input ID for top model item number: id="_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:0:AP1:r2:0:r5:0:qryId1:value00::content"
+let searchBoxModelSuffix = '_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:0:AP1:r2:0:r5:0:qryId1:value00::content';
+
+// button ID for top model item number: id="_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:0:AP1:r2:0:r5:0:qryId1::search"
+let searchButtonModelSuffix = '_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:0:AP1:r2:0:r5:0:qryId1::search';
+
+// input ID for PIO part number: id="_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:1:AP1:ATT1:_ATTp:it1::content"
+let searchBoxPioPartNumber = '_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:1:AP1:ATT1:_ATTp:it1::content';
+
+// A (href link for button) ID for PIO part number search button: id="_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:1:AP1:ATT1:_ATTp:HierarchyFilter"
+let searchButtonPioPartNumber = '_FOpt1:_FOr1:0:_FOSritemNode_pricing_administration_pricing_administration:0:MAt2:1:AP1:ATT1:_ATTp:HierarchyFilter';
+
